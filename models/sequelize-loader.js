b9be4a712b5763453ef2f6026e922257cc6ab150
fiore57 <fiore57.cpp@gmail.com>
2021-01-30 20:37:29 +0900

    Use ssl

--- models/sequelize-loader.js
+++ models/sequelize-loader.js
@@ -3,7 +3,13 @@ const Sequelize = require('sequelize');
 const sequelize = new Sequelize(
   process.env.DATABASE_URL || 'postgres://postgres:postgres@localhost/kk_typing',
   {
-    logging: false // ログを出力しない
+    logging: false, // ログを出力しない
+    production: {
+        dialect: "postgres",
+        dialectOptions: {
+            ssl: true
+        }
+    }
   }
 );
 
