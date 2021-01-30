d32e99960d422956cc974d2c7aac4ff2b556c074
fiore57 <fiore57.cpp@gmail.com>
2021-01-30 20:53:28 +0900

    u

--- models/sequelize-loader.js
+++ models/sequelize-loader.js
@@ -1,10 +1,15 @@
 'use strict';
-require('pg').defaults.ssl = true; // SSL を有効にする
 const Sequelize = require('sequelize');
 const sequelize = new Sequelize(
   process.env.DATABASE_URL || 'postgres://postgres:postgres@localhost/kk_typing',
   {
     logging: false, // ログを出力しない
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
   }
 );
 
