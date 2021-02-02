e1c9ba4f1b09269e5addf2bf6bf760e1d0f78ed5
fiore57 <fiore57.cpp@gmail.com>
2021-02-03 04:04:39 +0900

    u

--- models/sequelize-loader.js
+++ models/sequelize-loader.js
@@ -3,7 +3,7 @@ const Sequelize = require('sequelize');
 const sequelize = new Sequelize(
   process.env.DATABASE_URL || 'postgres://postgres:postgres@localhost/kk_typing',
   {
-    logging: false, // ログを出力しない
+    // logging: false, // ログを出力しない
     dialectOptions: {
       ssl: {
         require: true,
