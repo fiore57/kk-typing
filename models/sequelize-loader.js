df02392220611b4f03ddd2c25068379f399c3487
fiore57 <fiore57.cpp@gmail.com>
2021-02-03 04:29:33 +0900

    u

--- models/sequelize-loader.js
+++ models/sequelize-loader.js
@@ -1,8 +1,24 @@
 'use strict';
 const Sequelize = require('sequelize');
-const sequelize = new Sequelize(
-  process.env.DATABASE_URL || 'postgres://postgres:postgres@localhost/kk_typing',
-  {
+let sequelize;
+if (process.env.NODE_ENV === 'production' || (!process.env.POSTGRES_HOST || !process.env.POSTGRES_PORT)) {
+  sequelize = new Sequelize(
+    process.env.DATABASE_URL || 'postgres://postgres:postgres@localhost/kk_typing',
+    {
+      logging: false, // ログを出力しない
+      dialectOptions: {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false
+        }
+      }
+    }
+  );
+}
+else {
+  sequelize = new Sequelize('kk_typing', 'postgres', 'postgres', {
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
     // logging: false, // ログを出力しない
     dialectOptions: {
       ssl: {
@@ -10,8 +26,8 @@ const sequelize = new Sequelize(
         rejectUnauthorized: false
       }
     }
-  }
-);
+  });
+}
 
 module.exports = {
   database: sequelize,
